2bbf1151a4abf81f79786303395ddca4fb7892e5
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-07-31 21:19:40 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -33,7 +33,8 @@ export const App = () => {
   }
 
   return (
-    <Router basename={process.env.REACT_APP_PUBLIC_URL}>
+    <Router basename={process.env.PUBLIC_URL}>
+      {/* <Router> */}
       <div>
         <Header theme={theme} toggleTheme={toggleTheme} />
         <Section>
