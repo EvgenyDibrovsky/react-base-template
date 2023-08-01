1083682be4526825d95193ac5c375fce9854f478
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-08-01 20:50:52 +0200

    commit

--- src/components/SelectAuthForm.jsx
+++ src/components/SelectAuthForm.jsx
@@ -3,20 +3,20 @@ export default function SelectAuthForm({ selectedForm, setSelectedForm }) {
     <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-1 gap-1 my-5">
       <button
         onClick={() => setSelectedForm('Login')}
-        className={`h-10 flex items-center justify-center rounded-md border transition-all duration-200 border-btnAuthForm text-btnAuthForm hover:text-white hover:bg-btnAuthForm ${
+        className={`h-10 flex items-center justify-center rounded-md border transition-all duration-200 border-btnAuthForm dark:border-yellow-500 text-btnAuthForm dark:text-yellow-500 hover:text-white hover:bg-btnAuthForm hover:dark:bg-yellow-500 hover:dark:text-black ${
           selectedForm === 'Login'
-            ? 'bg-btnAuthForm text-white' // Удалить запятую
-            : 'text-btnAuthForm'
+            ? 'bg-btnAuthForm text-white dark:bg-yellow-500 dark:text-black' // Удалить запятую
+            : 'text-btnAuthForm dark:text-black'
         }`}
       >
         Login
       </button>
       <button
         onClick={() => setSelectedForm('Register')}
-        className={`h-10 flex items-center justify-center rounded-md border transition-all duration-200 border-btnAuthForm text-btnAuthForm hover:text-white hover:bg-btnAuthForm ${
+        className={`h-10 flex items-center justify-center rounded-md border transition-all duration-200 border-btnAuthForm dark:border-yellow-500 text-btnAuthForm dark:text-yellow-500 hover:text-white hover:bg-btnAuthForm hover:dark:bg-yellow-500 hover:dark:text-black ${
           selectedForm === 'Register'
-            ? 'bg-btnAuthForm text-white' // Удалить запятую
-            : 'text-btnAuthForm'
+            ? 'bg-btnAuthForm text-white  dark:bg-yellow-500 dark:text-black' // Удалить запятую
+            : 'text-btnAuthForm dark:text-black'
         }`}
       >
         Registration
